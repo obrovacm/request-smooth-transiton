c162fe047897fabb57745a6dc44540b575c0bc24
milos.obrovac <obrovac.milos@gmail.com>
2020-03-20 20:15:17 +0100

    cleaner code, better timing

--- index.js
+++ index.js
@@ -13,39 +13,39 @@ let b4 = document.querySelector("#b4");
 
 //////////////////////////////////////////////////////////////////
 
-function scrollHorizontaly(target, duration){  
+function scrollHorizontaly(target, duration){
+  // centering viewport on the target 
   let targetWidthHalf = target.getBoundingClientRect().width / 2;
   let halfScreen = window.innerWidth / 2;
-  // let targetPosition = target.getBoundingClientRect().left;
   let targetPosition = target.getBoundingClientRect().left - halfScreen + targetWidthHalf;
+  // viewport's starting scroll position
   let startPosition = window.pageXOffset;
+  requestAnimationFrame(scrollAnimation);
   
+  // scroll animation definition
   let startTime, timeElapsed, animationDevelopmentAtMoment;
   function scrollAnimation(currentTime) {
     if (!startTime) startTime = currentTime;
     timeElapsed = currentTime - startTime;
 
     animationDevelopmentAtMoment = ease(timeElapsed, startPosition, targetPosition, duration);
-    console.log("timeElapsed",timeElapsed, "\n duration", duration)
-    console.log("targetPosition",targetPosition, "\n animationDevelopmentAtMoment", animationDevelopmentAtMoment)
     html.scrollTo(animationDevelopmentAtMoment, 0);
     if(timeElapsed < duration) requestAnimationFrame(scrollAnimation);
   } 
-
+  // ease function definition
   function ease(t, b, c, d) { // http://www.gizma.com/easing/#quad1
     t /= d/2;
     if (t < 1) return c/2*t*t + b;
     t--;
     return -c/2 * (t*(t-2) - 1) + b;
   };
-  
-  requestAnimationFrame(scrollAnimation);
 }
 
 //////////////////////////////////////////////////////////////////
+const scrollingTime = 1700;
 
-b1.addEventListener("click", ()=>{scrollHorizontaly(k1, 1500)});
-b2.addEventListener("click",  ()=>{scrollHorizontaly(k2, 2000)});
-b3.addEventListener("click",  ()=>{scrollHorizontaly(k3, 800)});
-b4.addEventListener("click",  ()=>{scrollHorizontaly(k4, 1000)});
+b1.addEventListener("click", ()=>{scrollHorizontaly(k1, scrollingTime)});
+b2.addEventListener("click",  ()=>{scrollHorizontaly(k2, scrollingTime)});
+b3.addEventListener("click",  ()=>{scrollHorizontaly(k3, scrollingTime)});
+b4.addEventListener("click",  ()=>{scrollHorizontaly(k4, scrollingTime)});
 
