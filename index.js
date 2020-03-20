a04dfec6dcf55436f00569d1a875b1bde15db0fa
milos.obrovac <obrovac.milos@gmail.com>
2020-03-20 19:03:49 +0100

    kvadrat_4 + screen_center

--- index.js
+++ index.js
@@ -4,18 +4,20 @@ let html = container.parentElement.parentElement;
 let k1 = document.querySelector("#k1");
 let k2 = document.querySelector("#k2");
 let k3 = document.querySelector("#k3");
+let k4 = document.querySelector("#k4");
 
 let b1 = document.querySelector("#b1");
 let b2 = document.querySelector("#b2");
 let b3 = document.querySelector("#b3");
+let b4 = document.querySelector("#b4");
 
 //////////////////////////////////////////////////////////////////
 
 function scrollHorizontaly(target, duration){  
-  let targetCenter = target.getBoundingClientRect().width / 2;
+  let targetWidthHalf = target.getBoundingClientRect().width / 2;
   let halfScreen = window.innerWidth / 2;
   // let targetPosition = target.getBoundingClientRect().left;
-  let targetPosition = target.getBoundingClientRect().left - halfScreen + targetCenter;
+  let targetPosition = target.getBoundingClientRect().left - halfScreen + targetWidthHalf;
   let startPosition = window.pageXOffset;
   
   let startTime, timeElapsed, animationDevelopmentAtMoment;
@@ -24,7 +26,8 @@ function scrollHorizontaly(target, duration){
     timeElapsed = currentTime - startTime;
 
     animationDevelopmentAtMoment = ease(timeElapsed, startPosition, targetPosition, duration);
-
+    console.log("timeElapsed",timeElapsed, "\n duration", duration)
+    console.log("targetPosition",targetPosition, "\n animationDevelopmentAtMoment", animationDevelopmentAtMoment)
     html.scrollTo(animationDevelopmentAtMoment, 0);
     if(timeElapsed < duration) requestAnimationFrame(scrollAnimation);
   } 
@@ -42,6 +45,7 @@ function scrollHorizontaly(target, duration){
 //////////////////////////////////////////////////////////////////
 
 b1.addEventListener("click", ()=>{scrollHorizontaly(k1, 1500)});
-b2.addEventListener("click",  ()=>{scrollHorizontaly(k2, 1000)});
-b3.addEventListener("click",  ()=>{scrollHorizontaly(k3, 1000)});
+b2.addEventListener("click",  ()=>{scrollHorizontaly(k2, 2000)});
+b3.addEventListener("click",  ()=>{scrollHorizontaly(k3, 800)});
+b4.addEventListener("click",  ()=>{scrollHorizontaly(k4, 1000)});
 
