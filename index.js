221c2154f5989a9a11c7ac58db5763c237a611b1
milos.obrovac <milos.obrovac@etondigital.com>
2020-03-20 13:09:47 +0100

    request animation frame practice

---
+++ index.js
@@ -0,0 +1,44 @@
+let container = document.querySelector(".container");
+let html = container.parentElement.parentElement;
+
+let k1 = document.querySelector("#k1");
+let k2 = document.querySelector("#k2");
+let k3 = document.querySelector("#k3");
+
+let b1 = document.querySelector("#b1");
+let b2 = document.querySelector("#b2");
+let b3 = document.querySelector("#b3");
+
+//////////////////////////////////////////////////////////////////
+
+function scrollHorizontaly(target, duration){  
+  let targetPosition = target.getBoundingClientRect().left;
+  let startPosition = window.pageXOffset;
+  
+  let startTime, timeElapsed, animationDevelopmentAtMoment;
+  function scrollAnimation(currentTime) {
+    if (!startTime) startTime = currentTime;
+    timeElapsed = currentTime - startTime;
+
+    animationDevelopmentAtMoment = ease(timeElapsed, startPosition, targetPosition, duration);
+
+    html.scrollTo(animationDevelopmentAtMoment, 0);
+    if(timeElapsed < duration) requestAnimationFrame(scrollAnimation);
+  } 
+
+  function ease(t, b, c, d) { // http://www.gizma.com/easing/#quad1
+    t /= d/2;
+    if (t < 1) return c/2*t*t + b;
+    t--;
+    return -c/2 * (t*(t-2) - 1) + b;
+  };
+  
+  requestAnimationFrame(scrollAnimation);
+}
+
+//////////////////////////////////////////////////////////////////
+
+b1.addEventListener("click", ()=>{scrollHorizontaly(k1, 1500)});
+b2.addEventListener("click",  ()=>{scrollHorizontaly(k2, 1000)});
+b3.addEventListener("click",  ()=>{scrollHorizontaly(k3, 1000)});
+
